05e39bc2faaa82d5f16e80fd0dbabc4b2209db87
IT5406021630014 <5406021630014@fitm.kmutnb.ac.th>
2016-02-09 21:40:19 +0700

    profile

--- lab5-9.php
+++ lab5-9.php
@@ -4,7 +4,7 @@
   <title>Create form for recieve value</title>
 </head>
 <body>
-  <form method="POST" action="lab5-9cal.php">
+  <form method="POST" action="lab5-9cal.php" id="profile">
     <table border="1" align="center" width="500">
       <tr>
         <td colspan="2" align="center">
@@ -13,7 +13,14 @@
         <tr>
         <tr>
           <td>Enter name : </td>
-          <td><input type="text" name="name" size="50" value=""/></td>
+          <td>
+			<select name="front">
+			<option  value="นาย">นาย</option>
+			<option  value="นาง">นาง</option>
+			<option  value="นางสาว">นางสาว</option>
+			</select>
+			<input type="text" name="name" size="50" value=""/>
+		  </td>
         </tr>
           <td>Enter surname : </td>
           <td><input type="text" name="surname" size="50" value=""/></td>
@@ -26,14 +33,34 @@
         <tr>
           <td>Gender : </td>
           <td align="left">
-            <input type="radio" name="gen" value="Male">Male
-            <input type="radio" name="gen" value="Female">Female<br/>
+            <input type="radio" name="gen" value="1">Male
+            <input type="radio" name="gen" value="2">Female<br/>
           </td>
         </tr>
-          <td>Enter birthday : Ex dd/mm/yyyy</td>
-          <td><input type="text" name="day" size="2" value=""/> /
+          <td>Enter birthday : dd/mm/yyyy </td>
+          <td>
+		  <input type="text" name="day" size="2" value=""/> /
           <input type="text" name="mon" size="2" value=""/> /
-          <input type="text" name="year" size="4" value=""/></td>
+          <select name="year">
+		  <option value="">-----</option>
+		  <option value="1970">1970</option><option value="1971">1971</option><option value="1972">1972</option>
+		  <option value="1973">1973</option><option value="1974">1974</option><option value="1975">1975</option>
+		  <option value="1976">1976</option><option value="1977">1977</option><option value="1978">1978</option>
+		  <option value="1979">1979</option>
+		  <option value="1980">1980</option><option value="1981">1981</option><option value="1982">1982</option>
+		  <option value="1983">1983</option><option value="1984">1984</option><option value="1985">1985</option>
+		  <option value="1986">1986</option><option value="1987">1987</option><option value="1988">1988</option>
+		  <option value="1989">1989</option><option value="1990">1990</option><option value="1991">1991</option>
+		  <option value="1992">1992</option><option value="1993">1993</option><option value="1994">1994</option>
+		  <option value="1995">1995</option><option value="1996">1996</option><option value="1997">1997</option>
+		  <option value="1998">1998</option><option value="1999">1999</option><option value="2000">2000</option>
+		  <option value="2001">2001</option><option value="2002">2002</option><option value="2003">2003</option>
+		  <option value="2004">2004</option><option value="2005">2005</option><option value="2006">2006</option>
+		  <option value="2007">2007</option><option value="2008">2008</option><option value="2009">2009</option>
+		  <option value="2010">2010</option><option value="2011">2011</option><option value="2012">2012</option>
+		  <option value="2013">2013</option><option value="2014">2014</option><option value="2015">2015</option>
+		  <option value="2016">2016</option>
+		  </select>
         </tr>
         <tr>
           <tr>
@@ -42,8 +69,18 @@
         </tr>
         <tr>
         <td>Enter hobby : </td>
-        <td><input type="text" name="hob" size="30" value=""/></td>
-        </tr>
+        <td><input type="checkbox" name="c1" value="อ่านหนังสือ "/>อ่านหนังสือ
+			<input type="checkbox" name="c2" value="ดูหนัง "/>ดูหนัง <br/>
+			<input type="checkbox" name="c3" value="ฟังเพลง "/>ฟังเพลง
+			<input type="checkbox" name="c4" value="เล่นเกม "/>เล่นเกม<br/>
+			<input type="checkbox" name="c5" value="วาดรูป "/>วาดรูป
+			<input type="checkbox" name="c6" value="แต่งเพลง "/>แต่งเพลง<br/>
+			<input type="checkbox" name="c7" value="เขียนโปรแกรม "/>เขียนโปรแกรม
+			<input type="checkbox" name="c8" value="เล่นเฟสบุค "/>เล่นเฟสบุค<br/>
+			<input type="checkbox" name="c9" value="Steamer "/>Steamer
+			<input type="checkbox" name="c10" value="เขียนนิยาย "/>เขียนนิยาย<br/>
+		</td>
+		</tr>
           <td>Enter phonenumber : </td>
           <td><input type="text" name="cel" size="10" value=""/></td>
         </tr>
--- lab5-9cal.php
+++ lab5-9cal.php
@@ -1,13 +1,86 @@
 <html>
+<head>
+<meta charset="UTF-8">
+</head>
 <body>
 <?php
+  $front = $_POST['front'];
   $name = $_POST['name'];
   $surname = $_POST['surname'];
   $nm = $_POST['nm'];
   $gen = $_POST['gen'];
-  $birth = $_POST['birth'];
+  if($gen = 1)
+  {
+	  $gen = ชาย;
+  }
+  else
+  {
+	  $gen = หญิง;
+  }
+  $day = $_POST['day'];
+  $mon = $_POST['mon'];
+  $mond = intval($mon);
+  if($mon == 1)
+  {
+	  $mon = มกราคม;
+  }
+  else if($mon == 2)
+  {
+	  $mon =กุมภาพันธ์;
+  }
+  else if($mon == 3)
+  {
+	  $mon =มีนาคม;
+  }
+  else if($mon == 4)
+  {
+	  $mon =เมษายม;
+  }
+  else if($mon == 5)
+  {
+	  $mon =พฤษภาคม;
+  }
+  else if($mon == 6)
+  {
+	  $mon =มิถุนายน;
+  }
+  else if($mon == 7)
+  {
+	  $mon =กรกฎาคม;
+  }
+  else if($mon == 8)
+  {
+	  $mon =สิงหาคม;
+  }
+  else if($mon == 9)
+  {
+	  $mon =กันยายน;
+  }
+  else if($mon == 10)
+  {
+	  $mon =ตุลาคม;
+  }
+  else if($mon == 11)
+  {
+	  $mon =พฤษศจิกายน;
+  }
+  else if($mon == 12)
+  {
+	  $mon =ธันวาคม;
+  }
+  $year = $_POST['year'];
+  $y = intval($year)+543;
   $age = $_POST['age'];
-  $hob = $_POST['hob'];
+  $c1 = $_POST['c1'];
+  $c2 = $_POST['c2'];
+  $c3 = $_POST['c3'];
+  $c4 = $_POST['c4'];
+  $c5 = $_POST['c5'];
+  $c6 = $_POST['c6'];
+  $c7 = $_POST['c7'];
+  $c8 = $_POST['c8'];
+  $c9 = $_POST['c9'];
+  $c10 = $_POST['c10'];
   $cel = $_POST['cel'];
   $mail = $_POST['mail'];
   $add = $_POST['add'];
@@ -16,30 +89,27 @@
   echo "<tr>";
   echo "<p>";
   echo "<td>";
-  echo "Name : $name <br /><hr>";
-  echo "Surname : $surname <br /><hr>";
-  echo "Nickname : $nm <br /><hr>";
-  echo "Gender : $gen <br /><hr>";
-  echo "Birthdate : $birth <br /><hr>";
+  echo "$front &nbsp";
+  echo "$name &nbsp";
+  echo "$surname $nbsp";
+  echo "( $nm )<br /><hr>";
+  echo "เพศ : $gen <br /><hr>";
+  echo "เกิดเมื่อ : วันที่ $day เดือน $mon พ.ศ. ".$y."<br/><hr>";
 //---------------------------------------
-  echo "Age : $age ";
+  echo "อายุ : $age ";
   if(($age <= 0)/*||($age = 100)*/) echo "  <font color='red'>&nbsp | Your age is wrong.</font><br/><hr>";
-  elseif(($age > 0)&&($age<=60)) echo "  <font color='blue'>&nbsp | You are in working age.</font><br/><hr>";
+  elseif(($age > 0)&&($age<=13)) echo "  <font color='blue'>&nbsp | You are child.</font><br/><hr>";
+  elseif(($age > 13)&&($age<=59)) echo "  <font color='blue'>&nbsp | You are working age.</font><br/><hr>";
   elseif(($age > 60)&&($age<=100)) echo "  <font color='orange'>&nbsp | You aer retire.</font><br/><hr>";
   elseif($age > 100) echo "  <font color='red'>&nbsp | Your age is wrong</font><br/><hr>.";
 //---------------------------------------
-  echo "Hobby : $hob<br/><hr>";
-  echo "Phone number : $cel <br /><hr>";
-  echo "Email : $mail <br/><hr>";
-  echo "Address : $add <br/<hr>";
+  echo "งานอดิเรก : $c1 $c2 $c3 $c4 $c5 $c6 $c7 $c8 $c9 $c10 <br/><hr>";
+  echo "หมายเลขโทรศัพท์ : $cel <br /><hr>";
+  echo "อีเมล์ : $mail <br/><hr>";
+  echo "ที่อยู่ : $add <br/<hr>";
   echo "</td></tr>";
   //echo "----------------------<br/ >";
   echo "</table>";
-  /*if($operator == "+") echo($number1 + $number2)."</i><br />";
-  if($operator == "-") echo($number1 - $number2)."</i><br />";
-  if($operator == "*") echo($number1 * $number2)."</i><br />";
-  if($operator == "/") echo($number1 / $number2)."</i><br />";
-  if($operator == "%") echo($number1 % $number2)."</i><br />";*/
 ?>
 <br />
 <center><a href="lab5-9.php"><font size="3" color="BLUE">BACK</font></a></center>
